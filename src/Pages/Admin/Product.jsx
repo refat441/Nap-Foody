f1d10e1f6467fef5b83c626ab7b760254213a7f9
refatahmed441 <refatahmed441@gmail.com>
2024-12-16 21:08:20 +0600

    status added

--- src/Pages/Admin/Product.jsx
+++ src/Pages/Admin/Product.jsx
@@ -92,6 +92,9 @@ function Product() {
                 Name
               </th>
               <th className="px-3 py-2 md:px-6 md:py-3 text-left text-xs md:text-sm font-semibold">
+                Status
+              </th>
+              <th className="px-3 py-2 md:px-6 md:py-3 text-left text-xs md:text-sm font-semibold">
                 Price
               </th>
               <th className="px-3 py-2 md:px-6 md:py-3 text-left text-xs md:text-sm font-semibold">
@@ -104,76 +107,90 @@ function Product() {
           </thead>
           <tbody className="text-gray-700">
             {products.length > 0 ? (
-              products.map((product) => (
-                <tr
-                  key={product.id}
-                  className="border-b hover:bg-gray-100 transition duration-300 ease-in-out"
-                >
-                  <td className="px-3 md:px-6 py-2 text-xs md:text-sm ">
-                    {product.id}
-                  </td>
-                  <td className="px-3 md:px-6 py-2 text-xs md:text-sm">
-                    {product.name}
-                  </td>
-                  <td className="px-3 md:px-6 py-2 text-xs md:text-sm ">
-                    {product.price}
-                  </td>
-                  <td className="px-3 md:px-6 py-2 text-xs md:text-sm">
-                    <img
-                      src={`https://sunny.napver.com/storage/${product.product_image}`}
-                      alt={product.name}
-                      className="w-20 h-20 object-cover rounded shadow"
-                    />
-                  </td>
-                  <td className="px-3 py-4 text-xs md:text-sm flex items-center space-x-2 sm:space-x-3">
-                    <button
-                      disabled={toggleStatusLoading === product.id}
-                      onClick={() =>
-                        toggleProductStatus(product.id, product.status)
-                      }
-                      className={`flex items-center justify-center gap-2 px-2 py-1 sm:px-4 sm:py-2 text-xs sm:text-sm font-medium rounded-full shadow-md transition-all duration-300 ${
-                        product.status === "1"
-                          ? "bg-green-100 text-green-700 hover:bg-green-200"
-                          : "bg-red-100 text-red-700 hover:bg-red-200"
-                      } disabled:opacity-50 disabled:cursor-not-allowed`}
-                    >
-                      {toggleStatusLoading === product.id ? (
-                        <svg
-                          className="w-4 h-4 sm:w-5 sm:h-5 animate-spin"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <circle
-                            className="opacity-25"
-                            cx="12"
-                            cy="12"
-                            r="10"
-                            stroke="currentColor"
-                            strokeWidth="4"
-                          ></circle>
-                          <path
-                            className="opacity-75"
-                            fill="currentColor"
-                            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                          ></path>
-                        </svg>
+              products.map((product) => {
+                const isActive = product.status === "1"; // Determine if product is active
+                return (
+                  <tr
+                    key={product.id}
+                    className="border-b hover:bg-gray-100 transition duration-300 ease-in-out"
+                  >
+                    <td className="px-3 md:px-6 py-2 text-xs md:text-sm">
+                      {product.id}
+                    </td>
+                    <td className="px-3 md:px-6 py-2 text-xs md:text-sm">
+                      {product.name}
+                    </td>
+                    <td className="px-3 md:px-6 py-2 text-xs md:text-sm">
+                      {isActive ? (
+                        <span className="text-green-500 font-semibold">
+                          Active
+                        </span>
                       ) : (
-                        <>
-                          <span
-                            className={`w-2 h-2 sm:w-2.5 sm:h-2.5 rounded-full ${
-                              product.status === "1"
-                                ? "bg-green-500"
-                                : "bg-red-500"
-                            }`}
-                          ></span>
-                          {product.status === "1" ? "Active" : "Inactive"}
-                        </>
+                        <span className="text-red-500 font-semibold">
+                          Inactive
+                        </span>
                       )}
-                    </button>
-                  </td>
-                </tr>
-              ))
+                    </td>
+                    <td className="px-3 md:px-6 py-2 text-xs md:text-sm">
+                      {product.price}
+                    </td>
+                    <td className="px-3 md:px-6 py-2 text-xs md:text-sm">
+                      <img
+                        src={`https://sunny.napver.com/storage/${product.product_image}`}
+                        alt={product.name}
+                        className="w-20 h-20 object-cover rounded shadow"
+                      />
+                    </td>
+                    <td className="px-3 py-4 text-xs md:text-sm flex items-center space-x-2 sm:space-x-3">
+                      <button
+                        disabled={toggleStatusLoading === product.id}
+                        onClick={() =>
+                          toggleProductStatus(product.id, product.status)
+                        }
+                        className={`flex items-center justify-center gap-2 px-2 py-1 sm:px-4 sm:py-2 text-xs sm:text-sm font-medium rounded-full shadow-md transition-all duration-300 ${
+                          product.status === "1"
+                            ? "bg-green-100 text-green-700 hover:bg-green-200"
+                            : "bg-red-100 text-red-700 hover:bg-red-200"
+                        } disabled:opacity-50 disabled:cursor-not-allowed`}
+                      >
+                        {toggleStatusLoading === product.id ? (
+                          <svg
+                            className="w-4 h-4 sm:w-5 sm:h-5 animate-spin"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle
+                              className="opacity-25"
+                              cx="12"
+                              cy="12"
+                              r="10"
+                              stroke="currentColor"
+                              strokeWidth="4"
+                            ></circle>
+                            <path
+                              className="opacity-75"
+                              fill="currentColor"
+                              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                            ></path>
+                          </svg>
+                        ) : (
+                          <>
+                            <span
+                              className={`w-2 h-2 sm:w-2.5 sm:h-2.5 rounded-full ${
+                                product.status === "1"
+                                  ? "bg-green-500"
+                                  : "bg-red-500"
+                              }`}
+                            ></span>
+                            {product.status === "1" ? "Active" : "Inactive"}
+                          </>
+                        )}
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
             ) : (
               <tr>
                 <td colSpan="6" className="text-center py-4 text-gray-500">
